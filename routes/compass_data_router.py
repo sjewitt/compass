5bee70ee25cf6464c50441f6b3c5d9eecd111ed1
Silas <silas@thecoven.co.uk>
2026-03-05 17:16:14 +0000

    various route additions and extensiond to interacti with new models

--- routes/compass_data_router.py
+++ routes/compass_data_router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from api.models import Quadrant,QuadrantTitles,Sector,SectorTitles
+from api.models import Quadrant, QuadrantTitles,Sector,SectorTitles,CompassData, CompassDefinition, CompassSummary
 from api.db_models import DB_Quadrant, DB_QuadrantTitles,DB_Sector,DB_SectorTitles
 from api.database.engine import get_engine
 from api.database import handlers
@@ -12,9 +12,35 @@ router = APIRouter(
 
 engine = get_engine()
 
-@router.post("/quadrants/")
-def add_quadrant(engine, quadrant:Quadrant):
-    breakpoint()
+@router.get("/")
+def get_data() -> list[CompassSummary]:
+    ''' Retrieve summary data for all compass models defined, return ID and title only  '''
+    result = handlers.get_all_compasses(engine)
+    return result
+
+@router.get("/{id}")
+def get_data(id) -> CompassData:
+    ''' retrieve the definition by ID and compose the actual data in the handler '''
+    result = handlers.get_compass(engine,id)
+    return result
+
+@router.post("/")
+def set_data(definition:CompassDefinition) -> CompassSummary:
+    print(definition)
+    result = handlers.set_compass(engine,definition)
+    # print(result)
+    return CompassSummary(id=result,name=definition.name)
+    # return {}
+
+
+# @router.post("/")
+# def set_data(compass_data:CompassData):
+#     pass
+
+@router.post("/quadrant/")
+def add_quadrant(quadrant:Quadrant):
+    # breakpoint()
+    print("IN ADD QUAD:")
     print(quadrant)
     # _titles = [QuadrantTitles]
     # for _title in quadrant.title:
@@ -25,26 +51,45 @@ def add_quadrant(engine, quadrant:Quadrant):
     #     quadrant_elem_coords = quadrant.quadrant_elem_coords,
     #     children = _titles
     # )
+    print("CALLING HANDLER:")
     result = handlers.add_quadrant(engine,quadrant)
+    print("ADD QUAD RESULT:")
     print(result)
     pass
 
 @router.get("/quadrants/")
 def get_quadrants()->list[Quadrant]:
-    pass
+    print("CALLING HANDLERS:")
+    result = handlers.get_quadrants(engine)
+    print(result)
+    return result
 
 @router.get("/quadrants/{id}/")
 def get_quadrant(id:int)->Quadrant:
-    pass
+    result = handlers.get_quadrant(engine,id)
+    return result
 
-@router.get("/sectors/")
-def get_sectors()->list[Sector]:
-    pass
+# test:
+@router.get("/quadranttitles/")
+def get_quadrant_titles() -> list[QuadrantTitles]:
+    result = handlers.get_quadrant_titles(engine)
+    print(result)
+    return result
 
-@router.get("/sectors/{id}/")
-def get_sector(id:int)->Sector:
-    pass
 
 @router.post("/sectors/")
 def add_sector(sector:Sector):
-    pass
+    result = handlers.add_sector(engine,sector)
+    return result
+
+@router.get("/sectors/")
+def get_sectors()->list[Sector]:
+    result = handlers.get_sectors(engine)
+    return result
+
+@router.get("/sectors/{id}/")
+def get_sector(id:int)->Sector:
+    result = handlers.get_sector(engine, id)
+    return result
+
+
