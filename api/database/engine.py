ba7964a4497e78721d414d7a4d53fba4b18d2a09
Silas <silas@thecoven.co.uk>
2026-03-08 19:07:14 +0000

    turns off sqlalchemy engine logging

--- api/database/engine.py
+++ api/database/engine.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 # declare SQLite DB for persistent storage:
 DATABASE_URI = "sqlite:///./database/db.sqlite"
-engine = create_engine(DATABASE_URI, echo=True)
+engine = create_engine(DATABASE_URI, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # https://fastapitutorial.com/blog/dependencies-in-fastapi-coursefor-book/
