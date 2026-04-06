b7b9db8928b328b72b3d8f1221c59403051a4624
Silas <silas@thecoven.co.uk>
2026-04-06 16:51:20 +0100

    removes option for retrieving data from file

--- utilities/data_utilities.py
+++ utilities/data_utilities.py
@@ -28,27 +28,27 @@ def load_config_data(source:DataSource=DataSource['DATABASE'], engine=None, call
     # print(source)
     
     # THIS CAN GO...
-    if source == DataSource.FILESYSTEM:
-        # This is the container filesystem path:
-        with open(mode="r",file="/code/static/data/display_data_rationalised.json") as display_data:
-            compass_config_data["configuration"] = json.load(display_data)
+    # if source == DataSource.FILESYSTEM:
+    #     # This is the container filesystem path:
+    #     with open(mode="r",file="/code/static/data/display_data_rationalised.json") as display_data:
+    #         compass_config_data["configuration"] = json.load(display_data)
+    #         compass_config_data["status"] = "set"
+
+    # if source == DataSource.DATABASE:
+    with Session(engine) as session:
+        try:
+            # TO SORT:
+            if compass_id:
+                compass_config_data["configuration"] = handlers.get_compass(engine,compass_id)
+            else:
+                compass_config_data["configuration"] = handlers.get_compass(engine,1)
             compass_config_data["status"] = "set"
+            # # print("DATABASE DRIVEN COMPASS DATA")
+            # # _test = session.query(DB_Competency).all()
+            # # print(_test)
+            # print("set")
 
-    if source == DataSource.DATABASE:
-        with Session(engine) as session:
-            try:
-                # TO SORT:
-                if compass_id:
-                    compass_config_data["configuration"] = handlers.get_compass(engine,compass_id)
-                else:
-                    compass_config_data["configuration"] = handlers.get_compass(engine,2)
-                compass_config_data["status"] = "set"
-                # # print("DATABASE DRIVEN COMPASS DATA")
-                # # _test = session.query(DB_Competency).all()
-                # # print(_test)
-                # print("set")
-
-            except Exception as ex:
-                print(f"error: {ex}")
+        except Exception as ex:
+            print(f"error: {ex}")
 
     return compass_config_data
