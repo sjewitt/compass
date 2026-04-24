869bfe5754b26593a5e2ada0ee36c6d4ee2f5621
Silas <silas@thecoven.co.uk>
2026-04-24 13:15:13 +0100

    various changes to make IN objects without IDs

--- api/models.py
+++ api/models.py
@@ -41,76 +41,62 @@ class UserCompetencies(BaseModel):
 '''
 Model the compass lookup data
 '''
-class SectorTitles(BaseModel):  # RENAME!!
-    id: int = Field()
-    # sector_id:int = Field()
+class SectorTitlesIn(BaseModel):  # RENAME!!
     title_part:str = Field()
-    coord_x:int=Field()
-    coord_y:int=Field()
 
-class Sector(BaseModel):
-    id:int = Field()
-    # quadrant_id:int=Field(default_factory=0) # this can probably go?
-    title : list[SectorTitles] = Field(max_length=2, default_factory=list)
+class SectorTitles(SectorTitlesIn):
+    id: int = Field()
+
+class SectorIn(BaseModel):
     summary:str=Field()
     description:str=Field()
 
+class Sector(SectorIn):
+    id:int = Field()
+    title : list[SectorTitles] = Field(max_length=2, default_factory=list)
 
-class QuadrantTitles(BaseModel):
-    id: int = Field()   # I DO NOT NEED THIS!!
-    # quadrant_id:int=Field() # WTF?
+
+class QuadrantTitlesIn(BaseModel):
     title_part:str=Field()
-    coord_x:int  = Field()
-    coord_y:int  = Field()
+
+class QuadrantTitles(QuadrantTitlesIn):
+    id: int = Field()   # I DO NOT NEED THIS!!
     # override the built-in dunder method so we get a better print:
     def __repr__(self):
         return "<QuadrantTitles(id='%s', title_part='%s')>" % (
             self.id,
             self.title_part
         )
-
+    
 # https://stackoverflow.com/questions/76398690/how-can-i-prevent-an-unknown-id-field-from-showing-in-fastapi-documentation-when
-# HAH!! Also, see the oreilly course I am doing. He does exactly this!
-# class _QuadrantId(BaseModel):
-#     id: int = Field()
+# Can I adapt the handler to bypass the nested model? It doesn't need it as I can pass an empty array...
+# We also DO NOT need the CSS and coords as these are handled as constants (also, ties in with DB changes TODO:)
+class QuadrantIn(BaseModel):
+    quadrant_summary: str = Field()
 
-# class _QuadrantBase(BaseModel):
-#     # title : list[QuadrantTitles] = Field(max_length=2)
-#     quadrant_summary: str = Field()
-#     quadrant_css_class:str = Field()
-#     quadrant_elem_coords:str = Field()
+# exclude the optional sector and title list:
+class QuadrantBase(QuadrantIn):
+    id: int = Field()
 
-# class QuadrantIn(_QuadrantBase):
-#     title : list[QuadrantTitlesIn] = Field(max_length=2)
-#     # pass
-
-# class Quadrant(_QuadrantBase, _QuadrantId):
-#     title : list[QuadrantTitles] = Field(max_length=2)
-#     # pass
-
-class Quadrant(BaseModel):
+class Quadrant(QuadrantIn):
     id: int = Field()
     # https://stackoverflow.com/questions/63793662/how-to-give-a-pydantic-list-field-a-default-value
     title : list[QuadrantTitles] = Field(max_length=2,default_factory=list)
-    quadrant_summary: str = Field()
-    quadrant_css_class:str = Field()
-    quadrant_elem_coords:str = Field()
-    # sectors:list[Sector] = Field(min_length=4, max_length=5,default_factory=list)
-    # begin decouple data at higher level:
     sectors:list[Sector] = Field(max_length=5,default_factory=list)
 
+# Don't need an 'in' model for this:
 class CompassSummary(BaseModel):
     id:int = Field()
     name:str = Field(min_length=4, max_length=128)
 
 
 class RatingIn(BaseModel):
-    # ID?
     title:str = Field()
     description:str = Field()
 
 class Rating(RatingIn):
     id:int = Field()    # the database ID, which we will need for lookups etc.
+
 
 class CompassData(BaseModel):
     id:int=Field(default_factory=0)
@@ -124,15 +110,8 @@ class QuadrantDefinition(BaseModel):
     sectors:list[int] = Field(min_length=4, max_length=5)
 
 # map to actual database model:
-class CompassDefinition(BaseModel):
+class CompassDefinitionIn(BaseModel):
     name:str = Field(min_length=4, max_length=128)
-    # TO CHECK:
-    # # these are all ints (IDs of the relevant quadrants and sectors)
-    # quadrants:list[QuadrantDefinition] = Field(min_length=4, max_length=4)
-    # ratings:list[int] = Field(min_length=7, max_length=7)   # these are Ratig IDs
-    # # titles: list[QuadrantTitles] = Field(min_length=4, max_length=4)
-
-    id:int = Field()
     # Specify the quadrants (4)
     quadrant_1 : int = Field()
     quadrant_2 : int = Field()
@@ -222,3 +201,5 @@ class CompassDefinition(BaseModel):
     rating_6 : int = Field()
     rating_7 : int = Field()
 
+class CompassDefinition(CompassDefinitionIn):
+    id:int = Field()
