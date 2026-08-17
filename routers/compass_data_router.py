853676a5e68a382d0e00298c1239db05d878c677
Smeghammer <smeghammer@live.com>
2026-08-17 16:08:46 +0100

    removes many comments, rationalises some of the API handlers to directly return the call to the handlers; This last triggered the JS update of the event handlers for sectors, which was not working in FF

--- routers/compass_data_router.py
+++ routers/compass_data_router.py
@@ -3,12 +3,10 @@ import logging
 from fastapi import APIRouter, Response, status
 
 from api.models import Quadrant, QuadrantIn, QuadrantBase, QuadrantTitles, \
-        QuadrantTitlesIn, Sector, SectorIn, \
+        QuadrantTitlesIn, Sector, SectorIn, SectorEdit, \
         SectorTitles,SectorTitlesIn,CompassData, CompassDefinition, CompassDefinitionIn, CompassSummary, \
         Rating, RatingIn, APIResponseMessage
 from api.exceptions import CompassForUserNotFound, CompassDefinitionIncomplete
-# from api.db_models import DB_Quadrant, DB_QuadrantTitles,\
-#         DB_Sector,DB_SectorTitles, DB_Rating
 from api.database.engine import get_engine
 from api.database import handlers
 
@@ -32,11 +30,7 @@ def get_data() -> list[CompassSummary]:
     except Exception as ex:
         print(ex)
 
-#################
-# This is the initial entrypoint.
-# Currently, {id} is hardcoded to 2 in the javascript,
-# so I need to allow a compass ID to be passed from the currently
-# logged-in/selected user. TODO: 
+ 
 @router.get("/{id}", response_model=CompassData|APIResponseMessage)
 def get_data(id:int, response: Response) -> CompassData:
     ''' retrieve the definition by ID and compose the actual data in the handler '''
@@ -47,14 +41,13 @@ def get_data(id:int, response: Response) -> CompassData:
 
         return result
     except CompassForUserNotFound as ex:
-        print(ex)
-        # return None
+        # see https://fastapi.tiangolo.com/advanced/response-change-status-code/#use-case
+        # and https://fastapi.tiangolo.com/reference/status/#fastapi.status.HTTP_301_MOVED_PERMANENTLY
         response.status_code=status.HTTP_404_NOT_FOUND
         return APIResponseMessage(message=f"Compass with id '{id}' not found", success=False, source="get_data", data={}, status_code=status.HTTP_404_NOT_FOUND)
     except Exception as ex:
-        print(ex)
-        # return handlers.get_compass(engine,0)   # return a dummy to prevent errors (TO FIX PROPERLY!)
         return APIResponseMessage(message=f"Error occurred while fetching compass with id '{id}'", success=False, source="get_data", data={})
+
 
 @router.post("/")
 def set_data(definition:CompassDefinitionIn, response:Response) -> CompassSummary|APIResponseMessage:
@@ -89,133 +82,110 @@ def add_quadrant(quadrant:QuadrantIn):
 
 @router.post("/quadrant/update/")
 def update_quadrant(quadrant:QuadrantBase):
-    ''' 
-    ** Update a quadrant\n
-     The sector array can be empty (API will be updated to not require this at all at some point - models are in flux...)
-    '''
     result = handlers.update_quadrant(engine,quadrant)
     return result
 
-# retrieve quads, without  assigned titles
+
 @router.get("/quadrants/")
 def get_quadrants()->list[Quadrant]:
-    ''' get all quadrants defined in the database '''
     result = handlers.get_quadrants(engine)
     return result
 
-# # retrieve quads, without  assigned titles
-# @router.get("/quadrants/no_titles/")
-# def get_quadrants()->list[Quadrant]:
-#     result = handlers.get_quadrants(engine,include_titles=False)
-#     return result
-
-
-# retrieve all quadrant titles (as list, not assigned (rename this endpoint?))
 
 @router.post("/quadrants/title/")
 def add_quadrant_title(quadrant_title:QuadrantTitlesIn) -> QuadrantTitles:
-    ''' get all quadrant titles in the database '''
     result = handlers.add_quadrant_title(engine,quadrant_title)
     return result
 
+
 @router.post("/quadrants/title/update/")
 def update_quadrant_title(quadrant_title:QuadrantTitles) -> QuadrantTitles:
-    ''' get all quadrant titles in the database '''
     result = handlers.update_quadrant_title(engine,quadrant_title)
     return result
 
 
 @router.get("/quadrants/titles/")
 def get_quadrant_titles() -> list[QuadrantTitles]:
-    ''' get all quadrant titles in the database '''
     result = handlers.get_quadrant_titles(engine)
     return result
+
 
 @router.get("/quadrants/{id}/", response_model=QuadrantBase)
 def get_quadrant(id:int)->QuadrantBase:
     result = handlers.get_quadrant(engine,id)
     return result
 
+
 ################################################
 # SECTOR TITLES
-# (method order is important here!)
 ################################################
 @router.get("/sectors/titles/")
 def get_sector_titles() -> list[SectorTitles]:
-    ''' get all sector titles defined in the database '''
     result = handlers.get_sector_titles(engine)
     return result
 
+
 @router.get("/sectors/titles/{id}")
 def get_sector_title(id:int) -> SectorTitles:
-    ''' 
-    ## get specified sector title defined in the database 
-    '''
     result = handlers.get_sector_title(engine, id)
     return result
 
+
 @router.post("/sectors/title/update")
 def update_sector_title(updated_sector_title:SectorTitles) -> SectorTitles:
-    ''' ## get specified sector title defined in the database '''
     result = handlers.update_sector_title(engine, updated_sector_title)
     return result
 
+
 @router.post("/sectors/title/")
 def add_sector_title(sectortitle:SectorTitlesIn) -> bool:
-    '''
-      ## Add sector title\n 
-      Expects a `SectorTitles` instance
-    '''
     result = handlers.add_sector_title(engine,sectortitle)
     return result
 
+
 ################################################
-# SECTORS
+# SECTORS 
 ################
 @router.post("/sectors/")
 def add_sector(sector:SectorIn):
-    result = handlers.add_sector(engine,sector)
-    return result
+    return handlers.add_sector(engine,sector)
 
+
+# TODO: The model should not have the empty array any more.
+# See https://github.com/sjewitt/compass/issues/178
 @router.post("/sectors/update/")
-def update_sector(sector:Sector):
-    result = handlers.update_sector(engine,sector)
-    return result
+def update_sector(sector:SectorEdit):
+    return handlers.update_sector(engine,sector)
+
 
 @router.get("/sectors/")
 def get_sectors()->list[Sector]:
-    ''' get all sectors defined in the database '''
-    result = handlers.get_sectors(engine)
-    return result
+    return handlers.get_sectors(engine)
 
 
 @router.get("/sectors/{id}/")
 def get_sector(id:int)->Sector:
-    ''' get a sector by database ID '''
-    result = handlers.get_sector(engine, id)
-    return result
-
+    return handlers.get_sector(engine, id)
 
 
 ################
-# RATINGS
+# RATINGS TODO: add response models!
 ################
 @router.post("/rating/")
 def add_rating(rating:RatingIn):
-    result = handlers.add_rating(engine, rating)
-    return result
+    return handlers.add_rating(engine, rating)
+
 
 @router.post("/rating/update")
 def update_rating(rating:Rating):
-    result = handlers.update_rating(engine, rating)
-    return result
+    return handlers.update_rating(engine, rating)
+
 
 @router.get("/ratings/", response_model=list[Rating])
 def get_ratings()->list[Rating]:
-    result = handlers.get_ratings(engine)
-    return result
+    return handlers.get_ratings(engine)
+
 
 @router.get("/ratings/{id}", response_model=Rating)
 def get_ratings(id:int)->Rating:
-    result = handlers.get_rating(engine, id)
-    return result
+    return handlers.get_rating(engine, id)
